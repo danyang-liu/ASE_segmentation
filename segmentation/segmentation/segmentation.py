e099da000baf4924287c29134532b56d6cd2c0b1
v-danyl <v-danyl@microsoft.com>
2016-10-08 10:21:15 +0800

    1

---
+++ segmentation/segmentation/segmentation.py
@@ -0,0 +1,62 @@
+import os
+import shutil
+import nltk
+
+def get_n_topwords(dir_path, n):
+    filenames = os.listdir(dir_path)
+    words_frequences = {}
+    n = int(n)
+    for filename in filenames:
+        filepath = dir_path+'/'+filename
+        if os.path.isfile(filepath):
+            infile = open(filepath,'r')
+            pattern = r"""(?x)                   # set flag to allow verbose regexps
+                (?:[A-Z]\.)+           # abbreviations, e.g. U.S.A.
+                |\d+(?:\.\d+)?%?       # numbers, incl. currency and percentages
+                |\w+(?:[-']\w+)*       # words w/ optional internal hyphens/apostrophe
+                |\.\.\.                # ellipsis
+                """
+            try:
+                for line in infile:
+                    line_words = nltk.regexp_tokenize(line,pattern)
+                    for word in line_words:
+                        if word in words_frequences:
+                            words_frequences[word] = words_frequences[word] + 1
+                        else:
+                            words_frequences[word] = 1
+            except:
+                pass
+        else:
+            dir_path_2 = filepath
+            filenames_2 = os.listdir(dir_path_2)
+            for filename in filenames_2:
+                filepath = dir_path_2+'/'+filename
+                infile = open(filepath,'r')
+                pattern = r"""(?x)                   # set flag to allow verbose regexps
+                    (?:[A-Z]\.)+           # abbreviations, e.g. U.S.A.
+                    |\d+(?:\.\d+)?%?       # numbers, incl. currency and percentages
+                    |\w+(?:[-']\w+)*       # words w/ optional internal hyphens/apostrophe
+                    |\.\.\.                # ellipsis
+                    """
+                try:
+                    for line in infile:
+                        line_words = nltk.regexp_tokenize(line,pattern)
+                        for word in line_words:
+                            if word in words_frequences:
+                                words_frequences[word] = words_frequences[word] + 1
+                            else:
+                                words_frequences[word] = 1
+                except:
+                    pass
+    sortedwords = sorted(words_frequences.items(), key=lambda d: d[1], reverse=True)
+    i=0
+    while i < n:
+        print(sortedwords[i][0],' ',sortedwords[i][1])
+        i = i + 1
+
+
+if __name__ == '__main__':
+    dir = input('Root:')
+    n = input('N:')
+    get_n_topwords(dir, n)
+    print('finish!')
